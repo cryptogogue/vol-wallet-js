9dc735abc61619095a70cfc8a92f41a1051c28e9
cmorenoserrano <cmorenoserrano@sonatype.com>
2020-08-24 22:03:05 +0100

    Update TransactionFormController_SendVOL.js

--- src/TransactionFormController_SendVOL.js
+++ src/TransactionFormController_SendVOL.js
@@ -39,5 +39,9 @@ export class TransactionFormController_SendVOL extends TransactionFormController
 		if ( this.fields.amount.value < 0) {
 			this.fields.amount.error = 'Amount cannot be a negative number.';
 		}
+		
+		if ( this.fields.amount.value != parseInt(this.fields.amount.value,10)) {
+			this.fields.amount.error = 'Amount must be a positive integer';
+		}
     }
 };
