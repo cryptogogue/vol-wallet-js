547a9bdf2ef08e5deb30af44570573b263dd8977
Patrick Meehan <patrickmeehan.git@fallguyconsulting.com>
2021-12-11 21:24:27 -0800

    removed filter from asset log view

--- src/AccountLogView.js
+++ src/AccountLogView.js
@@ -11,69 +11,18 @@ import { observer }         from 'mobx-react';
 import * as UI              from 'semantic-ui-react';
 
 //================================================================//
-// Filter
-//================================================================//
-class Filter {
-
-    //----------------------------------------------------------------//
-    constructor ( entries ) {
-
-        this.unfiltered = entries;
-    }
-
-    //----------------------------------------------------------------//
-    @computed get
-    filtered () {
-
-        let index = 0;
-        const filtered = [];
-        for ( let entry of this.unfiltered ) {
-
-            switch ( entry.type ) {
-                case TRANSACTION_TYPE.ACCOUNT_POLICY:
-                case TRANSACTION_TYPE.AFFIRM_KEY:
-                case TRANSACTION_TYPE.BETA_GET_ASSETS:
-                case TRANSACTION_TYPE.BETA_GET_DECK:
-                case TRANSACTION_TYPE.BUY_ASSETS:
-                case TRANSACTION_TYPE.CANCEL_OFFER:
-                case TRANSACTION_TYPE.KEY_POLICY:
-                case TRANSACTION_TYPE.OFFER_ASSETS:
-                case TRANSACTION_TYPE.OPEN_ACCOUNT:
-                case TRANSACTION_TYPE.PUBLISH_SCHEMA:
-                case TRANSACTION_TYPE.PUBLISH_SCHEMA_AND_RESET:
-                case TRANSACTION_TYPE.REGISTER_MINER:
-                case TRANSACTION_TYPE.RENAME_ACCOUNT:
-                case TRANSACTION_TYPE.RESERVE_ACCOUNT_NAME:
-                case TRANSACTION_TYPE.RUN_SCRIPT:
-                case TRANSACTION_TYPE.SEND_ASSETS:
-                case TRANSACTION_TYPE.SEND_VOL:
-                case TRANSACTION_TYPE.STAMP_ASSETS:
-                case TRANSACTION_TYPE.UPGRADE_ASSETS:
-                    filtered.push ( index );
-                    break;
-            }
-            index++;
-        }
-        return filtered;
-    }
-};
-
-//================================================================//
 // AccountLogView
 //================================================================//
 export const AccountLogView = observer (( props ) => {
     
+    const { entries }           = props;
     const accountService        = props.accountService;
-    const filter                = hooks.useFinalizable (() => new Filter ( props.entries ));
-    const pagingController      = hooks.useFinalizable (() => new PagingController ( filter.filtered.length ));
-
-    const filtered              = filter.filtered;
-    const entries               = props.entries;
+    const pagingController      = hooks.useFinalizable (() => new PagingController ( entries.length ));
 
     let transactionList = [];
     for ( let i = pagingController.pageItemMin; i < pagingController.pageItemMax; ++i ) {
 
-        const index         = filtered [ filtered.length - i - 1 ];
+        const index         = entries.length - i - 1;
         const entry         = entries [ index ];
         const isUnread      = ( !entry.isMaker && ( index >= accountService.inboxRead ));
 
